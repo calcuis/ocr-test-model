2da8282069142f083d17d124602717897688d96a
calcuis <113646141+calcuis@users.noreply.github.com>
2025-06-22 02:50:19 +0800

    Update file_converter.py

--- core/file_converters/file_converter.py
+++ core/file_converters/file_converter.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from abc import ABC
 from abc import abstractmethod
 
-
 class FileConverter(ABC):
     @abstractmethod
     def convert_to_images(self, file_path: str):
